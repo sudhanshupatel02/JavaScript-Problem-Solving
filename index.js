d3e13f04c518b162b1d95298f3b1540bb298a81f
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-06 13:45:16 +0530

    Hoisting and variables functions

---
+++ index.js
@@ -0,0 +1,42 @@
+//! ************** 1 Method *************
+// Hoisting and  variables _ functions
+
+       //! variables _ functions)
+console.log(x); //undefined
+var x = 7;
+console.log(x); //7
+
+//! Hoisting :-
+// function ko pehle call kar skte h, phir uska declaration     (or)
+// function ko pehle declaration kar skte h, phir uska call ise hi Hoisting
+
+// getName(); //Problem Solving
+
+// function getName(){
+//         console.log("Problem Solving");
+//     }
+
+// getName(); //Problem Solving
+
+
+//Arrow function ko any other variable ki trah behaviour(yah functoin ki trah behaviour nhi karta)
+getName();  //getName is not a function
+
+var getName=()=>{
+        console.log("Problem Solving");
+    }
+
+getName(); //Problem Solving
+    
+//! ************** 2 Method *************
+
+
+
+    //! ************** 1 Method *************
+    //! ************** 1 Method *************
+
+
+
+
+
+
